1ef0b7c0ee6f36a458292e02ca5d2a083e55e0a7
JY.Yang <21738331@qq.com>
2026-05-11 15:35:20 +0800

    修复 amount 的问题

--- internal/http/handler/statements_handler.go
+++ internal/http/handler/statements_handler.go
@@ -391,24 +391,24 @@ func errInvalidParam(field string) error {
 }
 
 type statementWritePayload struct {
-	Type         string  `json:"type"`
-	Amount       float64 `json:"amount"`
-	Description  string  `json:"description"`
-	Mood         string  `json:"mood"`
-	CategoryID   int64   `json:"category_id"`
-	AssetID      int64   `json:"asset_id"`
-	FromAssetID  int64   `json:"from_asset_id"`
-	ToAssetID    int64   `json:"to_asset_id"`
-	PayeeID      int64   `json:"payee_id"`
-	TargetObject string  `json:"target_object"`
-	Location     string  `json:"location"`
-	Nation       string  `json:"nation"`
-	Province     string  `json:"province"`
-	City         string  `json:"city"`
-	District     string  `json:"district"`
-	Street       string  `json:"street"`
-	Date         string  `json:"date"`
-	Time         string  `json:"time"`
+	Type         string `json:"type"`
+	Amount       string `json:"amount"`
+	Description  string `json:"description"`
+	Mood         string `json:"mood"`
+	CategoryID   int64  `json:"category_id"`
+	AssetID      int64  `json:"asset_id"`
+	FromAssetID  int64  `json:"from_asset_id"`
+	ToAssetID    int64  `json:"to_asset_id"`
+	PayeeID      int64  `json:"payee_id"`
+	TargetObject string `json:"target_object"`
+	Location     string `json:"location"`
+	Nation       string `json:"nation"`
+	Province     string `json:"province"`
+	City         string `json:"city"`
+	District     string `json:"district"`
+	Street       string `json:"street"`
+	Date         string `json:"date"`
+	Time         string `json:"time"`
 }
 
 type statementWriteRequest struct {
@@ -422,9 +422,15 @@ func buildStatementWriteInput(c *gin.Context) (service.StatementWriteInput, erro
 	}
 
 	p := req.Statement
+
+	amount, err := strconv.ParseFloat(p.Amount, 64)
+	if err != nil {
+		return service.StatementWriteInput{}, errInvalidParam("amount")
+	}
+
 	return service.StatementWriteInput{
 		Type:         p.Type,
-		Amount:       p.Amount,
+		Amount:       amount,
 		Description:  p.Description,
 		Mood:         p.Mood,
 		CategoryID:   p.CategoryID,
